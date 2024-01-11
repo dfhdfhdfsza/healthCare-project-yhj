ca8b3959961451c6141818728126f21fe4ac0d3b
HANGYEOL\ffink <ffinky99@gmail.com>
2024-01-11 21:14:00 +0900

    asdsad

--- healthcare/www/src/main/java/com/healthcare/www/user/domain/UserInfo.java
+++ healthcare/www/src/main/java/com/healthcare/www/user/domain/UserInfo.java
@@ -1,38 +1,38 @@
-package com.healthcare.www.user.domain;
-
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@Entity
-public class UserInfo {
-
-  @Id
-  @OneToOne
-  @PrimaryKeyJoinColumn(name = "user_no")
-  private User userNo; // 유저 고유 번호
-
-  @Column(name="info_height")
-  private double infoHeight; // 유저 키
-
-  @Column(name="info_weight")
-  private double infoWeight; // 유저 몸무게
-
-  @Column(name="info_skeletal")
-  private double infoSkeletal; // 유저 골격근량
-
-  @Column(name="info_body")
-  private double infoBody; // 유저 체지방률
-
-  @Column(name="info_metabolic")
-  private double infoMetabolic; // 유저 기초대사량
-
-
-}
+//package com.healthcare.www.user.domain;
+//
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.ToString;
+//
+//@Getter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@ToString
+//@Entity
+//public class UserInfo {
+//
+//  @Id
+//  @OneToOne
+//  @PrimaryKeyJoinColumn(name = "user_no")
+//  private User userNo; // 유저 고유 번호
+//
+//  @Column(name="info_height")
+//  private double infoHeight; // 유저 키
+//
+//  @Column(name="info_weight")
+//  private double infoWeight; // 유저 몸무게
+//
+//  @Column(name="info_skeletal")
+//  private double infoSkeletal; // 유저 골격근량
+//
+//  @Column(name="info_body")
+//  private double infoBody; // 유저 체지방률
+//
+//  @Column(name="info_metabolic")
+//  private double infoMetabolic; // 유저 기초대사량
+//
+//
+//}
