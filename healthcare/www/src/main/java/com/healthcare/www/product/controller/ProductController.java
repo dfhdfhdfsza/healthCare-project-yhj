6821231c9d198b47d618720627bd17e8942e0498
MoonJinBae <munbe88@gmail.com>
2024-01-11 21:19:59 +0900

    커밋

--- healthcare/www/src/main/java/com/healthcare/www/product/controller/ProductController.java
+++ healthcare/www/src/main/java/com/healthcare/www/product/controller/ProductController.java
@@ -3,13 +3,16 @@ package com.healthcare.www.product.controller;
 import com.healthcare.www.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/product/**")
+@RequestMapping("/product/")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
+    @GetMapping("productRegister")
+    public void getProductRegister(){}
 }
