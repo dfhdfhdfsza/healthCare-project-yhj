bda59cbf3783f8685ed8c72d18936852b5a3b49d
HANGYEOL\ffink <ffinky99@gmail.com>
2024-01-12 20:07:57 +0900

    asd

--- healthcare/www/src/main/resources/static/js/common/nav.js
+++ healthcare/www/src/main/resources/static/js/common/nav.js
@@ -1,4 +1,5 @@
 let alarmStatus = 0;
+let showHeart = false;
 document.querySelectorAll('.nav_box_ul li:not(#alarm-li) i').forEach(function(icon) {
     icon.addEventListener('click', function() {
         var anchorElement = icon.nextElementSibling;
@@ -6,16 +7,16 @@ document.querySelectorAll('.nav_box_ul li:not(#alarm-li) i').forEach(function(ic
     });
 });
 
+//알람 li 눌렀을때 발생하는 이벤트
 document.getElementById('alarm-li').addEventListener('click', function() {
     var elementsToToggle = document.querySelectorAll('.nav_box_ul li:not(#alarm-li) a, #alarm-li a');
-     var logo = document.querySelector('.nav_logo');
      var navbox = document.querySelector('.nav_box');
      var alarmbox = document.getElementById('alarm-box');
      var alarmContent =document.getElementById('alarm-content');
+
     elementsToToggle.forEach(function(element) {
         if (alarmStatus === 0) {
             element.style.display = 'none';
-            logo.style.color = 'transparent';
             navbox.style.width = '70px';
              alarmbox.style.left ='70px';
              setTimeout(function() {
@@ -23,7 +24,6 @@ document.getElementById('alarm-li').addEventListener('click', function() {
               }, 300);
         } else {
             element.style.display = '';
-            logo.style.color = 'white';
             navbox.style.width = '250px';
             alarmbox.style.left = '-300px';
             alarmContent.style.display='none';
@@ -31,4 +31,33 @@ document.getElementById('alarm-li').addEventListener('click', function() {
     });
 
     alarmStatus = (alarmStatus === 0) ? 1 : 0;
+    toggleHeartIcon();
+    console.log(showHeart);
 });
+
+/** 하트버튼 선택 애니메이션  */
+ function toggleHeartIcon() {
+            var heartIcon = document.getElementById("heartIcon");
+            var houseIcon =document.getElementById("houseIcon");
+             var alarmLi = document.querySelector('#alarm-li')
+            if (showHeart) {
+                heartIcon.classList.remove("bi-heart-fill");
+                heartIcon.classList.add("bi-heart");
+                alarmLi.style.border = "none";
+                
+                houseIcon.classList.remove("bi-house");
+                houseIcon.classList.add("bi-house-fill");
+               
+            } else {
+                heartIcon.classList.remove("bi-heart");
+                heartIcon.classList.add("bi-heart-fill");
+                alarmLi.style.border = "solid 1px #aaa";
+
+                houseIcon.classList.remove("bi-house-fill");
+                houseIcon.classList.add("bi-house");
+            }
+
+
+            showHeart = !showHeart;
+        }
+
