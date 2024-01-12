7b3f45a27e467bc01caea24c920f0c0b98749331
leedonghoon0418 <dlehdgs@naver.com>
2024-01-12 19:48:40 +0900

    no message

--- healthcare/www/src/main/java/com/healthcare/www/config/SecurityConfig.java
+++ healthcare/www/src/main/java/com/healthcare/www/config/SecurityConfig.java
@@ -1,4 +1,25 @@
 package com.healthcare.www.config;
 
-public class SecurityConfig {
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfiguration {
+
+  public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
+
+
 }
