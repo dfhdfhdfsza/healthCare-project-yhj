4b4f4d0c8eee4cdc6c0e3efb265f1bbd684f6f22
leedonghoon0418 <dlehdgs@naver.com>
2024-01-13 18:00:31 +0900

    no message

--- healthcare/www/src/main/java/com/healthcare/www/config/SecurityConfig.java
+++ healthcare/www/src/main/java/com/healthcare/www/config/SecurityConfig.java
@@ -1,25 +1,33 @@
 package com.healthcare.www.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
+@EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig{
 
-  public PasswordEncoder passwordEncoder(){
-    return new BCryptPasswordEncoder();
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    // 인가작업
+    http.authorizeHttpRequests(auth->auth
+        .requestMatchers("/","/signup","/login").permitAll()
+        .requestMatchers("/admin").hasRole("ADMIN")
+        .anyRequest().authenticated());
+
+
+    return http.build();
   }
+
+
 
 
 }
